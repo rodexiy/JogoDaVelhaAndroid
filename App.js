44646e1a6f28e25d80181ac0ebebe98f5842e97d
Daniel L. Rocha <rodexcontato@gmail.com>
2023-09-19 21:42:44 -0300

    a
    a

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+
+  
+
+  const handleClick = (event) => {
+    alert("Click")
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
+
+      <Text>Nome player1: {player1}</Text>
+      <TextInput placeholder='player1' style={styles.input} onChangeText={setPlayer1}></TextInput>
+      <Text>Nome player2: {player2}</Text>
+      <TextInput placeholder='player2' style={styles.input} onChangeText={setPlayer2}></TextInput>
+
+      <Button title="Botao" onPress={handleClick}></Button>
     </View>
   );
 }
@@ -16,5 +35,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  input: {
+    width: "80%",
+    height: "20",
+    borderStyle: "solid",
+    borderColor: "black",
+    borderWidth: 1,
+    
+  },
+
 });
